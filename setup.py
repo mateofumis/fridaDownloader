3951ad944513d757170757b8e0add2154f3ab6ce
mateofumis <87695536+mateofumis@users.noreply.github.com>
2025-02-19 11:14:43 -0300

    update to v1.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fridaDownloader",
-    version="1.2.0",
+    version="1.3.0",
     packages=find_packages(),
     install_requires=[
         "requests==2.32.3",
